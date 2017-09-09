b68e713143cc2aaccef11b58d9433a5790112b59
vinitraj10 <ent.vinit01@gmail.com>
2017-09-10 02:21:38 +0530

    Blog

--- Backend/Blog/settings.py
+++ Backend/Blog/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
