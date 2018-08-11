0a22a200739d04ed27747c73d9b856579c4051d0
vinitraj10 <vinitraj1012@gmail.com>
2018-08-11 17:12:11 +0530

    Folder name changed

---
+++ Backend/config/urls.py
@@ -0,0 +1,28 @@
+"""Blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url,include
+from django.contrib import admin
+from v1.accounts.views import FacebookLogin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^blog/api/',include('v1.post.urls'),name='blog'),
+    url(r'^accounts/api/',include('v1.accounts.urls')),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login')
+
+]
