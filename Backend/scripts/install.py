2d7661b200897bb2ebb25d4b05099dfea5cb9f22
vinitraj10 <vinitraj1012@gmail.com>
2018-08-11 17:15:25 +0530

    scripts for ease purposes

---
+++ Backend/scripts/install.py
@@ -0,0 +1,11 @@
+'''
+    file for installation of project dependencies
+    for platform specific.
+
+    working on this file so ignore the content of
+    it for the time.
+
+'''
+import platform
+
+print(platform.system())
