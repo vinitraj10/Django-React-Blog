b5a4ea2ab100205c254c02f5940f43be4e764949
vinitraj10 <vinitraj1012@gmail.com>
2018-08-12 18:30:54 +0530

    config files fixed

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -165,6 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_HEADERS = [
