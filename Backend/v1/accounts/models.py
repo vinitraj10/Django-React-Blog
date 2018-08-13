45984e316cfc1121c520a44b47e0593431a6d8c9
vinitraj10 <vinitraj1012@gmail.com>
2018-08-13 23:21:53 +0530

    Models changed

--- Backend/v1/accounts/models.py
+++ Backend/v1/accounts/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
+class ArrayField:
+    def __init__(self):
+        self.values = []
+
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     college = models.CharField(max_length=200)
     picture = models.FileField(upload_to='profile_pic/')
-
+    skills = ArrayField()
     class Meta:
        verbose_name_plural='Profile'
 
