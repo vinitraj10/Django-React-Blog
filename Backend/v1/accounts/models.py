e94723536596da39dc1469feca342ddd8daf478e
vinitraj10 <vinitraj1012@gmail.com>
2018-08-15 20:39:57 +0530

    Profile Skill Model added

--- Backend/v1/accounts/models.py
+++ Backend/v1/accounts/models.py
@@ -2,17 +2,26 @@ from django.db import models
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
-class ArrayField:
-    def __init__(self):
-        self.values = []
-
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     college = models.CharField(max_length=200)
     picture = models.FileField(upload_to='profile_pic/')
-    skills = ArrayField()
     class Meta:
        verbose_name_plural='Profile'
 
     def __str__(self):
         return str(self.user.username)
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class HasSkill(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.profile.user.username) + ' ' + str(self.skill.name)
