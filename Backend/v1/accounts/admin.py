660eb5d0553f2e85ffa7d1980e430e1250866de4
vinitraj10 <vinitraj1012@gmail.com>
2018-08-15 20:41:34 +0530

    Model registered in admin panel

--- Backend/v1/accounts/admin.py
+++ Backend/v1/accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from v1.accounts.models import Profile
+from v1.accounts.models import Profile,Skill,HasSkill
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Skill)
+admin.site.register(HasSkill)
