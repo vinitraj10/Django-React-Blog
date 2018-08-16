2e1ac6ba52525a4dc32affb96dfdc530fa34203c
vinitraj10 <vinitraj1012@gmail.com>
2018-08-16 13:15:53 +0530

    urls file updated

--- Backend/v1/accounts/urls.py
+++ Backend/v1/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url,include
 from v1.accounts import views
 urlpatterns = [
-	url(r'^auth/register/',views.register),
-	url(r'^auth/login/',views.login),
-	url(r'^update/profile/',views.update_profile),
-	url(r'^getmyprofile/',views.get_my_profile),
+	url(r'^auth/register/$',views.register),
+	url(r'^auth/login/$',views.login),
+	url(r'^update/profile/$',views.update_profile),
+	url(r'^update/profile/skills/',views.update_profile_skills),
+	url(r'^getmyprofile/$',views.get_my_profile),
 ]
