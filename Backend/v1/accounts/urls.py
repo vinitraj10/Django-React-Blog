96518fc4fd5869b95b5a9cf1c2587a566aab53a6
vinitraj10 <vinitraj1012@gmail.com>
2018-08-18 14:04:26 +0530

    github oauth working

--- Backend/v1/accounts/urls.py
+++ Backend/v1/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from v1.accounts.views.signup import signup_view
 from v1.accounts.views.signin import signin_view
+from v1.accounts.views.oauth import github
 from v1.accounts.views.update_profile import update_profile_view
 from v1.accounts.views.update_profile_skills import update_profile_skills_view
 from v1.accounts.views.follow import follow_view
@@ -9,6 +10,7 @@ from v1.accounts.views.unfollow import unfollow_view
 urlpatterns = [
 	path('auth/signup/',signup_view),
 	path('auth/signin/',signin_view),
+	path('auth/github/',github),
 	path('update/profile/',update_profile_view),
 	path('update/profile/skills/',update_profile_skills_view),
 	path('follow/',follow_view),
--- Backend/v1/accounts/views/oauth.py
+++ Backend/v1/accounts/views/oauth.py
@@ -1,14 +1,56 @@
+import json
+import jwt
+import requests
+from datetime import datetime
+from decouple import config
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from v1.accounts.models import Profile
+
+# it will return token for accessing dev-circle
+def oauth_create_or_get_token(email):
+    try:
+        print(email)
+        user = User.objects.get(email=email)
+        print(user)
+        time_now = str(datetime.now())
+        payload = {
+            'email':user.email,
+            'time_now':time_now
+        }
+        jwt_token = jwt.encode(payload, 'secret', algorithm='HS256')
+        jwt_token = jwt_token.decode('utf-8')
+        return jwt_token
+    except:
+        time_now = str(datetime.now())
+        new_user = User()
+        new_user.email = email
+        new_user.save()
+        payload = {
+            'email':new_user.email,
+            'time_now':time_now
+        }
+        jwt_token = jwt.encode(payload, 'secret', algorithm='HS256')
+        jwt_token = jwt_token.decode('utf-8')
+        return jwt_token
 @csrf_exempt
 def github(req):
     code = req.GET['code']
-    client_id = '87f79de4f9f2a46069b7'
-    client_secret = '143386855d09a461bea81ec4b1563e961e3a613f'
+    CLIENT_ID = config('CLIENT_ID')
+    CLIENT_SECRET = config('CLIENT_SECRET')
     url = 'https://github.com/login/oauth/access_token/'
-    post_data = {
-                    'client_id':client_id,
-                    'client_secret':client_secret,
-                    'code':code
-                }
-    r = requests.post(url,data=json.dumps(post_data))
-    print(r.json())
-    return JsonResponse({})
+    headers = {'Accept': 'application/json'}
+    params = {
+        'client_id':CLIENT_ID,
+        'client_secret':CLIENT_SECRET,
+        'code':code
+    }
+    git_response = requests.post(url,headers=headers,params=params).json()
+    access_token = git_response['access_token']
+    user_url = 'https://api.github.com/user?access_token='+ access_token
+    git_user_response = requests.get(user_url).json()
+    email = git_user_response['email']
+    token = oauth_create_or_get_token(email)
+    print(token)
+    return JsonResponse({'token':token})
