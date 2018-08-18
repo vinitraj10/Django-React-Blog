03a8f5d78fd455053359f507c94b6ef446bd6683
vinitraj10 <vinitraj1012@gmail.com>
2018-08-18 07:36:36 +0530

    Github oauth added

--- Backend/v1/accounts/urls.py
+++ Backend/v1/accounts/urls.py
@@ -3,9 +3,9 @@ from v1.accounts import views
 urlpatterns = [
 	url(r'^auth/register/$',views.register),
 	url(r'^auth/login/$',views.login),
+	url(r'^auth/github/$',views.github),
 	url(r'^update/profile/$',views.update_profile),
 	url(r'^update/profile/skills/',views.update_profile_skills),
-	url(r'^getmyprofile/$',views.get_my_profile),
 	url(r'^follow/$',views.follow_profile),
-	url(r'^getuserprofile/')
+	url(r'^unfollow/$',views.unfollow_profile),
 ]
--- Backend/v1/accounts/views.py
+++ Backend/v1/accounts/views.py
@@ -1,5 +1,6 @@
 import json
 import jwt
+import requests
 from django.http import JsonResponse
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
@@ -11,6 +12,11 @@ from v1.accounts.validators.regvalidator import (
 
 from v1.accounts.validators.logvalidator import (
     check_credentials
+)
+
+from v1.accounts.validators.authenticate import (
+    verify_auth,
+    is_owner
 )
 
 from v1.accounts.models import (
@@ -44,7 +50,10 @@ def register(req):
             jwt_token = jwt_token.decode('utf-8')
             # Create User Profile
             Profile(user=new_user).save()
-            return JsonResponse({'token':jwt_token, 'username':new_user.username}, status=201)
+            return JsonResponse({
+                        'token':jwt_token,
+                        'username':new_user.username
+                    }, status=201)
         else:
             return JsonResponse({'error':"Email or username exists"})
     return JsonResponse({'error':'Method not allowed'})
@@ -82,46 +91,78 @@ def login(req):
 
 @csrf_exempt
 def update_profile(req):
-    if req.method == 'PUT':
-        data = json.loads(req.body)
-        username = data['username']
-        college = data['college']
-        picture = data['picture']
-        user = User.objects.get(username=username)
-        profile = Profile.objects.get(user=user)
-        profile.college = college
-        profile.picture = picture
-        profile.save()
-        return JsonResponse({'success':'Profile Updated'}, status=200)
-    return JsonResponse({'error':'Method Not Allowed'}, status=405)
+    data = json.loads(req.body)
+    try:
+        token = req.META['HTTP_AUTHORIZATION']
+        if(verify_auth(token) and is_owner(token,data['username'])):
+            if req.method == 'PUT':
+                data = json.loads(req.body)
+                username = data['username']
+                college = data['college']
+                picture = data['picture']
+                user = User.objects.get(username=username)
+                profile = Profile.objects.get(user=user)
+                profile.college = college
+                profile.picture = picture
+                profile.save()
+                return JsonResponse({
+                            'success':'Profile Updated'
+                        }, status=200)
+            return JsonResponse({
+                        'error':'Method Not Allowed'
+                        }, status=405)
+        else:
+            return JsonResponse({
+                        'error':'You are not the owner'
+                    })
+    except:
+        return JsonResponse({
+                        'error':'Please Login First'
+                    })
 
 @csrf_exempt
 def update_profile_skills(req):
-    if req.method == 'PUT':
-        data = json.loads(req.body)
-        username = data['username']
-        skill = data['skill']
-        user = User.objects.get(username=username)
-        profile = Profile.objects.get(user=user)
-        flag = skill_is_in_profile(skill, profile)
-        if flag[0]:
-            return JsonResponse({'skills':flag[1]}, status=200)
+    data = json.loads(req.body)
+    try:
+        token = req.META['HTTP_AUTHORIZATION']
+        if (verify_auth(token) and is_owner(token,data['username'])):
+            if req.method == 'PUT':
+                username = data['username']
+                skill = data['skill']
+                user = User.objects.get(username=username)
+                profile = Profile.objects.get(user=user)
+                flag = skill_is_in_profile(skill, profile)
+                if flag[0]:
+                    return JsonResponse({
+                            'skills':flag[1]
+                        }, status=200)
+                else:
+                    return JsonResponse({
+                                'error':flag[1]
+                        }, status=403)
+            elif req.method == 'DELETE':
+                data = json.loads(req.body)
+                username = data['username']
+                skill_id = data['skill_id']
+                user = User.objects.get(username=username)
+                profile = Profile.objects.get(user=user)
+                skill = Skill.objects.get(id=skill_id)
+                skill_from_profile = HasSkill.objects.get(
+                                        profile=profile,
+                                        skill=skill
+                                    )
+                skill_from_profile.delete()
+                return JsonResponse({
+                        'success':'Succesfully Deleted'
+                        }, status=200)
+            else:
+                return JsonResponse({'error':'Method Not Allowed'})
         else:
-            return JsonResponse({'error':flag[1]}, status=403)
-    elif req.method == 'DELETE':
-        data = json.loads(req.body)
-        username = data['username']
-        skill_id = data['skill_id']
-        user = User.objects.get(username=username)
-        profile = Profile.objects.get(user=user)
-        skill = Skill.objects.get(id=skill_id)
-        skill_from_profile = HasSkill.objects.get(profile=profile, skill=skill)
-        skill_from_profile.delete()
-        return JsonResponse({'success':'Succesfully Deleted'}, status=200)
-    else:
-        return JsonResponse({'error':'Method Not Allowed'})
+            JsonResponse({'error':'You are not the owner'})
+    except:
+        return JsonResponse({'error':'Please login first'})
 
-# Design a decorator for checking authentication
+'''
 def get_my_profile(req):
     user = User.objects.get(username='raj10')
     profile = Profile.objects.get(user=user)
@@ -136,24 +177,76 @@ def get_my_profile(req):
         'skills':skills
     }
     return JsonResponse(data, status=200)
-
+'''
 @csrf_exempt
 def follow_profile(req):
-    if req.method == 'POST':
-        data = json.loads(req.body)
-        username = data['username']
-        follower = data['follower']
-        profile = User.objects.get(username=username)
-        follower = User.objects.get(username=follower)
-        try:
-            following = Following.objects.get(profile=profile, follower=follower)
-            return JsonResponse({'msg':'already following'})
-        except Exception as e:
-            following = Following.objects.create(profile=profile, follower=follower)
-            return JsonResponse({'msg':'following'})
-    else:
-        return JsonResponse({'error':'Method Not Allowed'})
+    data = json.loads(req.body)
+    try :
+        token = req.META['HTTP_AUTHORIZATION']
+        if(verify_auth(token) and is_owner(token,data['follower'])):
+            if req.method == 'POST':
+                username = data['username']
+                follower = data['follower']
+                profile = User.objects.get(username=username)
+                follower = User.objects.get(username=follower)
+                try:
+                    following = Following.objects.get(
+                                    profile=profile,
+                                    follower=follower
+                                )
+                    return JsonResponse({'msg':'already following'})
+                except Exception as e:
+                    following = Following.objects.create(
+                                    profile=profile,
+                                    follower=follower
+                                )
+                    return JsonResponse({'msg':'following'})
+            else:
+                return JsonResponse({'error':'Method Not Allowed'})
+        else:
+            print("fired")
+            return JsonResponse({'error':'You are not the owner'})
+    except:
+        return JsonResponse({'error':'Please login first'})
 
-def get_user_profile(req):
+@csrf_exempt
+def unfollow_profile(req):
+    data = json.loads(req.body)
+    try:
+        token = req.META['HTTP_AUTHORIZATION']
+        if(verify_auth(token) and is_owner(token,data['username'])):
+            if req.method == 'DELETE':
+                data = json.loads(req.body)
+                username = data['username']
+                following = data['following']
+                profile = User.objects.get(username = following)
+                follower = User.objects.get(username = username)
+                try:
+                    unfollow = Following.objects.get(
+                                    profile=profile,
+                                    follower=follower
+                                )
+                    unfollow.delete()
+                    return JsonResponse({'message':'Unfollowed'+' '+following})
+                except:
+                    return JsonResponse({'error':'You do not follow the user'})
+            return JsonResponse({'error':'Method Not allowed'})
+        else:
+            return JsonResponse({'error':'You are not the owner'})
+    except:
+        return JsonResponse({'error':'Please login first'})
 
+@csrf_exempt
+def github(req):
+    code = req.GET['code']
+    client_id = '87f79de4f9f2a46069b7'
+    client_secret = '143386855d09a461bea81ec4b1563e961e3a613f'
+    url = 'https://github.com/login/oauth/access_token/'
+    post_data = {
+                    'client_id':client_id,
+                    'client_secret':client_secret,
+                    'code':code
+                }
+    r = requests.post(url,data=json.dumps(post_data))
+    print(r.json())
     return JsonResponse({})
