951c83a59647138b5561be56b5b95dbd6a22e6ab
vinitraj10 <vinitraj1012@gmail.com>
2018-08-19 14:14:24 +0530

    Login form container added

--- Frontend/src/containers/signin/index.js
+++ Frontend/src/containers/signin/index.js
@@ -3,10 +3,41 @@ import './css/login.css';
 
 const Signin = () => (
 	<div className="container">
-		<div className="columns">
-			<div className="column col-12">
-					<div className="login">
-							<h1>Login</h1>
+		<div className="columns content">
+			<div className="column col-6 col-sm-12 col-xs-12">
+					<div className="panel">
+							<div className="panel-header">
+								<div className="panel-title"><h3 className="text-center">Signin</h3></div>
+							</div>
+							<div className="panel-body">
+								<div className="form-group">
+									<label className="form-label" htmlFor="input-example-1">email</label>
+									<input
+											className="form-input"
+											type="text"
+											id="input-example-1"
+											placeholder="email"
+									/>
+								</div>
+								<div className="form-group">
+									<label className="form-label" htmlFor="input-example-1">password</label>
+									<input
+										className="form-input"
+										type="text"
+										id="input-example-1"
+										placeholder="password"
+									/>
+								</div>
+								<div className="form-group">
+									<button className="btn btn-default btn-lg">Login</button>
+								</div>
+							</div>
+						<div className="panel-footer">
+							<div className="divider text-center" data-content="OR" />
+							<div className="column col-12">
+								<button className="btn-si btn-github">Sign in with GitHub</button>
+							</div>
+						</div>
 					</div>
 			</div>
 		</div>
