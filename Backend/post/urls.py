df4554d450070f8b8012666730a95ecceca13f03
mentix02 <manan.yadav02@gmail.com>
2018-10-31 01:25:29 +0530

    added view to urls

--- Backend/post/urls.py
+++ Backend/post/urls.py
@@ -5,16 +5,18 @@ from post.views import(
 	PostDeleteView,
 	PostListView,
 	PostUpdateView,
+	AuthorPostView
 )
 
 app_name = 'post'
 
 urlpatterns =  [
 
-	url(r'^$',PostListView.as_view(),name='List'),
-	url(r'^create',PostCreateView.as_view(),name='Create'),
-	url(r'^detail/(?P<pk>[\d-]+)/$',PostDetailView.as_view(),name='Details'),
-	url(r'^update/(?P<pk>[\d-]+)/$',PostUpdateView.as_view(),name='Update'),
-	url(r'^delete/(?P<pk>[\d-]+)/$',PostDeleteView.as_view(),name='Delete'),
+	url(r'^$', PostListView.as_view(), name='List'),
+	url(r'^create', PostCreateView.as_view(), name='Create'),
+	url(r'^detail/(?P<pk>[\d-]+)/$', PostDetailView.as_view(), name='Details'),
+	url(r'^update/(?P<pk>[\d-]+)/$', PostUpdateView.as_view(), name='Update'),
+	url(r'^delete/(?P<pk>[\d-]+)/$', PostDeleteView.as_view(), name='Delete'),
+	url(r'^author/(?P<username>[-\w]+)/$', AuthorPostView.as_view(), name='Author-Post')
 
 ]
