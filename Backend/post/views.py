ff0a217deee32ee489d14b1bda11e9e462e79a2d
mentix02 <manan.yadav02@gmail.com>
2018-10-30 20:11:10 +0530

    cleaned up code according to PEP 8

--- Backend/post/views.py
+++ Backend/post/views.py
@@ -5,11 +5,9 @@ from rest_framework.generics import (
 	RetrieveAPIView,
 	RetrieveUpdateAPIView
 )
-
 from rest_framework.permissions import(
 	IsAuthenticatedOrReadOnly
 )
-
 from post.serializers import (
 	PostCreateSerializer,
 	PostListSerializer,
@@ -18,28 +16,33 @@ from post.serializers import (
 from post.models import Post
 from post.permissions import IsOwnerOrReadOnly
 
+
 class PostCreateView(CreateAPIView):
-	serializer_class=PostCreateSerializer
+	serializer_class = PostCreateSerializer
+
 	def perform_create(self,serializer):
 		serializer.save(author=self.request.user)
-		
+
+
 class PostDeleteView(DestroyAPIView):
-	queryset=Post.objects.all()
-	serializer_class=PostListSerializer
-	lookup_field='pk'
+	lookup_field = 'pk'
+	queryset = Post.objects.all()
+	serializer_class = PostListSerializer
+
 
 class PostListView(ListAPIView):
-	queryset=Post.objects.all()
-	serializer_class=PostListSerializer
+	queryset = Post.objects.all()
+	serializer_class = PostListSerializer
+
 
 class PostDetailView(RetrieveAPIView):
-	queryset=Post.objects.all()
-	serializer_class=PostListSerializer
-	lookup_field='pk'
+	lookup_field = 'pk'
+	queryset = Post.objects.all()
+	serializer_class = PostListSerializer
+
 
 class PostUpdateView(RetrieveUpdateAPIView):
-	queryset=Post.objects.all()
-	serializer_class=PostCreateSerializer
-	permission_classes = [IsOwnerOrReadOnly]
-	lookup_field='pk'
-
+	queryset = Post.objects.all()
+	serializer_class = PostCreateSerializer
+	permission_classes = (IsOwnerOrReadOnly, )
+	lookup_field = 'pk'
