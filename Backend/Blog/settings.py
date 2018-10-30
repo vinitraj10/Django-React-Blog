1875ef92c45b4111df9c5d1ce88153c38499f286
mentix02 <manan.yadav02@gmail.com>
2018-10-31 01:23:33 +0530

    set debug to False for development

--- Backend/Blog/settings.py
+++ Backend/Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w=9z=a##*hd2w$@qn!%2r@k!v66+c4-2y)z&o&1+u-b8+2kbq@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['f9ee65d3.ngrok.io','localhost','127.0.0.1']
 
