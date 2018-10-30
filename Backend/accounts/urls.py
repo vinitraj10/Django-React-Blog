67da25cfe93fa617e90cc45cd43af268c6544556
mentix02 <manan.yadav02@gmail.com>
2018-10-30 20:15:11 +0530

    cleaned code and added app_name

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import url,include
-
+from django.conf.urls import url, include
+from rest_framework_jwt.views import obtain_jwt_token
 from accounts.views import (
 	UserCreateView,
 
 )
-
-from rest_framework_jwt.views import obtain_jwt_token
 
 app_name = 'accounts'
 
