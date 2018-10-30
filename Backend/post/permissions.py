7bfb9c41d3fceff7bca470adfe699ca577868e4c
mentix02 <manan.yadav02@gmail.com>
2018-10-30 20:10:29 +0530

    lowercased message

--- Backend/post/permissions.py
+++ Backend/post/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import BasePermission
 
 class IsOwnerOrReadOnly(BasePermission):
-	message = "You Must be the Author of the post to perform this aaction"
-	def has_object_permission(self,request,view,obj):
+	message = 'You must be the author of the post to perform this action'
+	
+	def has_object_permission(self, request, view, obj):
 		return obj.author == request.user
