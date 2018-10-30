e7ea9d3f2b646ebe557a234137dc6216c17d5f03
mentix02 <manan.yadav02@gmail.com>
2018-10-30 20:12:57 +0530

    changed Meta ordering so that newer posts come first

--- Backend/post/models.py
+++ Backend/post/models.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.db import models
 
+
 class Post(models.Model):
-	author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
 	content = models.TextField(max_length=200)
 	published = models.DateTimeField(auto_now_add=True) 
+	author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
 
 	def __str__(self):
-		return str(self.title)
+		return self.title
+
+	class Meta:
+		ordering = ('-published', '-pk')
