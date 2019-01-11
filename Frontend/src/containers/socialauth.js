cdd859ff7d427334075bb926045cf87c79cb41b9
vinitraj10 <vinitraj1012@gmail.com>
2019-01-11 20:50:35 +0530

    Social-auth File name fixed

--- Frontend/src/containers/socialauth.js
+++ Frontend/src/containers/socialauth.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {loginUsingFb} from '../actions/authentication/social-auth';
+import {loginUsingFb} from '../actions/Authentication/social-auth';
 
 class SocialAuth extends Component{
   loadFbLoginApi() {
