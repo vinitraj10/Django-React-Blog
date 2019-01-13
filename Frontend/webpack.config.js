08ce5d4d0465691e4c50d5e7a4bc8492c3077de6
vinitraj10 <vinitraj1012@gmail.com>
2019-01-13 17:05:11 +0530

    Development variables updated

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -11,15 +11,29 @@ var config = {
 		publicPath:'/dist/js',
 		filename:'bundle.min.js'
 	},
-	module:{
-		loaders:[
-			{
-				test:/\.jsx?/,
-				include:APP_DIR,
-				loader:'babel-loader',
-				exclude:'/node_modules/',
-			}
-		]
+	module: {
+	    rules: [
+	      {
+	        // this is so that we can compile any React,
+	        // ES6 and above into normal ES5 syntax
+	        test: /\.(js|jsx)$/,
+	        // we do not want anything from node_modules to be compiled
+	        exclude: /node_modules/,
+	        use: ['babel-loader']
+	      },
+	      {
+	        test: /\.(css|scss)$/,
+	        use: [
+	          "style-loader", // creates style nodes from JS strings
+	          "css-loader", // translates CSS into CommonJS
+	          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+	        ]
+	      },
+	      {
+	        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+	        loaders: ['file-loader']
+	      }
+	    ]
 	},
 	devServer: {
 	    historyApiFallback: true,
