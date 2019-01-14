90e387a9eca012fad4381dba0c373b27e0651356
vinitraj10 <vinitraj1012@gmail.com>
2019-01-14 19:00:42 +0530

    airbnb-eslint style guide integrated

--- Frontend/src/actions/Authentication/index.js
+++ Frontend/src/actions/Authentication/index.js
@@ -8,47 +8,47 @@ import {
 
 const ROOT_URL = 'https://djminapi.herokuapp.com/accounts/api/';
 
-export function signup(formValue,callback){
-	const URL = `${ROOT_URL}register/`;
-	return (dispatch) =>{
-		axios.post(URL,formValue)
-		.then((response)=>{
-			const{username}= response.data;
-			console.log(username);
-			dispatch({type:SIGNUP_USER});
-			localStorage.setItem('token',response.data.token);
-			localStorage.setItem('username',username);
-			callback();
-		})
-		.catch((error)=>{
-			dispatch({type:AUTH_ERROR,payload:'ERROR OCCURED USERNAME MAY EXISTS IN DATABASE'});
-		})
-	}
-
+export function signup (formValue, callback) {
+  const URL = `${ROOT_URL}register/`;
+  return (dispatch) => {
+    axios.post(URL, formValue)
+      .then((response) => {
+        const { username } = response.data;
+        dispatch({ type: SIGNUP_USER });
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('username', username);
+        callback();
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch({ type: AUTH_ERROR, payload: 'ERROR OCCURED USERNAME MAY EXISTS IN DATABASE' });
+      });
+  };
 }
 
-export function signin(formValue,callback){
-	const URL =`${ROOT_URL}home/login/token/`
-	return (dispatch) => {
-		axios.post(URL,formValue)
-		.then((response)=>{
-			const {username}=response.data.user;
-			dispatch({type:AUTH_USER});
-			localStorage.setItem('token',response.data.token);
-			localStorage.setItem('username',username);
-			callback();
-		})
-		.catch((err)=>{
-			dispatch({type:AUTH_ERROR,payload:'BAD LOGIN CREDENTIALS'});
-		})
-	}
+export function signin (formValue, callback) {
+  const URL = `${ROOT_URL}home/login/token/`;
+  return (dispatch) => {
+    axios.post(URL, formValue)
+      .then((response) => {
+        const { username } = response.data.user;
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('username', username);
+        callback();
+      })
+      .catch((error) => {
+        console.log(error.response);
+        dispatch({ type: AUTH_ERROR, payload: 'BAD LOGIN CREDENTIALS' });
+      });
+  };
 }
 
-export function signout(callback){
-	localStorage.removeItem('token');
-	localStorage.removeItem('username');
-	return (dispatch) =>{
-		dispatch({type:UNAUTH_USER});
-		callback();
-	}
+export function signout (callback) {
+  localStorage.removeItem('token');
+  localStorage.removeItem('username');
+  return (dispatch) => {
+    dispatch({ type: UNAUTH_USER });
+    callback();
+  };
 }
